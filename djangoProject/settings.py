22d7a9c6a915cf248cd513170c9408d5066704db
Maksim <mdroid308@gmail.com>
2021-03-12 19:04:14 +0300

    Added requirements file

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 's+9-v^xa3)&&e^r2=8c=sn93e**kmk$q0s4+tq2cgldu3ozz*$'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,19 +69,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 
 DATABASES = {
     'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': 'SportDB',
-    'USER': 'postgres',
-    'PASSWORD': 'sports',
-    'HOST': '127.0.0.1',
-    'PORT': '5432'
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'SportDB',
+        'USER': 'postgres',
+        'PASSWORD': 'adminka',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
@@ -105,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -118,7 +114,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
