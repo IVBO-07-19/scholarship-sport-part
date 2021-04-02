60dc73c0c65a3d9a2a149874f25701421397b632
Maksim Shekhanin <61467290+Niccko@users.noreply.github.com>
2021-04-02 23:58:23 +0300

    Delete init_db.py

--- init_db.py
+++
@@ -1,6 +0,0 @@
-name = input('DB name: ')
-user = input('Username: ')
-password = input('User password: ')
-
-with open('djangoProject/.env', 'w') as env:
-    env.write(f'DATABASE_URL = postgres://{user}:{password}@localhost:5432/{name}')
