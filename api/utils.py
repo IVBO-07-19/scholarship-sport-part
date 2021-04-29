f5ca9b84c348c1043ea374cc41ea38360d4d6645
Maksim <mdroid308@gmail.com>
2021-04-29 18:39:28 +0300

    auth

--- api/utils.py
+++ api/utils.py
@@ -5,7 +5,6 @@ from django.contrib.auth import authenticate
 import json
 import jwt
 import requests
-import docx
 
 dotenv.read_dotenv()
 
@@ -28,30 +27,5 @@ def jwt_decode_token(token):
 
     issuer = 'https://{}/'.format(os.environ.get('JWT_ISSUER'))
     return jwt.decode(token, public_key, audience=os.environ.get('JWT_AUDIENCE'), issuer=issuer, algorithms=['RS256'])
-
-def create_table(data):
-    document = docx.Document()
-    document.add_paragraph('Получение в течение 1-ого года, предшествующего назначению повышенной государственной академической стипендии, награды\
-                            (приза) за результаты спортивной деятельности, осуществленной в рамках спортивных международных, всероссийских, ведомственных,\
-                            региональных мероприятий, проводимых Университетом или иной организацией')
-    object_data = json.loads(data)
-    table1 = document.add_table(rows=1,cols=6)
-    hdr_cells = table1.rows[0].cells
-    hdr_cells[0].text = 'Название мероприятия'
-    hdr_cells[1].text = 'Уровень(международное,всероссийское,ведомственное и региональное)'
-    hdr_cells[2].text = 'Степень участия(индивидуальное, командное)'
-    hdr_cells[3].text = 'Занятое место'
-    hdr_cells[4].text = 'Дата мероприятия'
-    hdr_cells[5].text = 'Баллы'
-    for i in object_data:
-        print(i['place'])
-        row_cells = table1.add_row().cells
-        row_cells[0].text = i['name']
-        row_cells[1].text = i['level']
-        row_cells[2].text = i['degree']
-        row_cells[3].text = str(i['place'])
-        row_cells[4].text = i['date']
-        row_cells[5].text = str(i['points'])
-    document.save('Global_event.docx')
 
 
