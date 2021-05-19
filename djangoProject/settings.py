6f38845aea0ac7dd521f5ccf74197be54b848c77
Maksim <mdroid308@gmail.com>
2021-05-19 19:12:55 +0300

    tests added

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -95,15 +95,11 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 #     }
 # else:
 
-if os.getenv('GITHUB_WORKFLOW'):
+if sys.argv[1] == 'test':
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'github-actions',
-            'USER': 'postgres',
-            'PASSWORD': 'postgres',
-            'HOST': 'localhost',
-            'PORT': '5432'
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 else:
