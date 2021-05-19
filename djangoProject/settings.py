a7cc8f7d38ee2ecd63aec482abe5caf854bbf88e
Maksim <mdroid308@gmail.com>
2021-05-19 18:13:45 +0300

    tests added

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import sys
 from pathlib import Path
 import dj_database_url
 import dotenv
@@ -85,21 +86,28 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-
-DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'NAME': 'SportDB',
-        # 'USER': 'postgres',
-        # 'PASSWORD': 'adminka',
-        # 'HOST': '127.0.0.1',
-        # 'PORT': '5432'
+if sys.argv[1] == 'test':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            # 'NAME': 'SportDB',
+            # 'USER': 'postgres',
+            # 'PASSWORD': 'adminka',
+            # 'HOST': '127.0.0.1',
+            # 'PORT': '5432'
+        }
+    }
 
-if os.environ.get('DATABASE_URL'):
-    db_from_env = dj_database_url.config()
-    DATABASES['default'].update(db_from_env)
+    if os.environ.get('DATABASE_URL'):
+        db_from_env = dj_database_url.config()
+        DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
